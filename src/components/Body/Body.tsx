259607273a4259ecba8880954d42ddf5e7d1675b
hyorish03 <hyorish03@naver.com>
2023-08-18 16:47:51 +0900

    Feat: 인스타그램 및 노션 바로가기 버튼

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -24,9 +24,57 @@ function Body() {
           </div>
         ))}
       </Section>
+      <ButtonWrapper>
+        <Button
+          type="button"
+          onClick={() => {
+            location.href = "https://www.instagram.com/seron_official/";
+          }}
+        >
+          인스타 바로가기
+        </Button>
+        <Button
+          type="button"
+          onClick={() => {
+            location.href = "https://url.kr/6c8i13";
+          }}
+        >
+          노션 바로가기
+        </Button>
+      </ButtonWrapper>
     </Wrapper>
   );
 }
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 50px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 10px;
+  }
+`;
+
+const Button = styled.button`
+  width: 150px;
+  height: 50px;
+  border: 2px solid white;
+  border-radius: 10px;
+  font-size: 20px;
+  font-weight: bold;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #74b0d8;
+    transition: 0.5s;
+    color: white;
+  }
+`;
 
 const Wrapper = styled.div`
   display: flex;
