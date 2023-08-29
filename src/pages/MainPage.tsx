d081c3eb50560340f3a7b6998ad5c8d8731f66dc
hyorish03 <hyorish2001@gmail.com>
2023-08-29 21:53:45 +0900

    Fix: 불필요한 import 제거

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-import Nav from "../components/Nav/Nav";
 import Body from "../components/Body/Body";
-import Footer from "../components/Nav/Footer";
 
 function MainPage() {
   return (
